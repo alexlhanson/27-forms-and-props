6829afa8be2fff008f963f9e4dac376612544c33
alexlhanson <alexlhanson@gmail.com>
2018-10-21 23:43:33 -0700

    Add bugfix for number of results

--- src/components/search/search-form.js
+++ src/components/search/search-form.js
@@ -26,7 +26,7 @@ export default class SearchForm extends React.Component{
   };
 
   handleLimit(e){
-    this.setState({limit: (e.target.value - 1) || 25});
+    this.setState({limit: (e.target.value) || 25});
   }
 
 
@@ -35,7 +35,7 @@ export default class SearchForm extends React.Component{
       <div className="redditSearch">
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.handleSearchText} placeholder="Search Reddit" className={this.props.className}></input>
-          <input onChange={this.handleLimit} placeholder="# Results" type="number" max="100" min="0"></input>
+          <input onChange={this.handleLimit} placeholder="# Results" type="number" max="99" min="1"></input>
           <button type="submit">Search</button>
         </form>
       </div>
